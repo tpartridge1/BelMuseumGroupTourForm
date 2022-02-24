33e567bd301b1c4bf03f6f430048ed3aeec1baa6
tpartridge1 <tammy,partridge7@gmail.com>
2022-02-24 17:41:30 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,11 @@
+//hides toggle in textarea when no or unsure is selected
+function show1() {
+	document.getElementById("sensdiv").style.display = "none";
+}
+
+//shows toggle in textarea when yes is selected
+function show2() {
+	document.getElementById("sensdiv").style.display = "block";
+}
+
+
